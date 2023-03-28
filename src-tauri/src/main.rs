fd79a405350c8155b0548fd7fab14bedce326e8c
Lorenzo Ravaglia <lorenzo.ravaglia@hope.edu>
2023-03-28 16:10:56 -0400

    feat: setup logger and error handler in main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,9 +8,13 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+fn main() -> color_eyre::Result<()> {
+    pretty_env_logger::try_init()?;
+    color_eyre::install()?;
+    
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .run(tauri::generate_context!())?;
+
+    Ok(())
 }
