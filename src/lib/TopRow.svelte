75cd5f62e1c22620706557149f9a598852fdc40b
Lorenzo Ravaglia <lorenzo.ravaglia@hope.edu>
2023-03-29 09:11:52 -0400

    feat: update default ui to a better default

---
+++ src/lib/TopRow.svelte
@@ -0,0 +1,35 @@
+<nav>
+  <ul>
+    <li><a href="/"><h3>Home</h3></a></li>
+    <li><a href="/about"><h3>About</h3></a></li>
+    <li><a href="/contact"><h3>Contact</h3></a></li>
+  </ul>
+</nav>
+
+<style>
+  nav {
+    background: #30638E;
+    color: #D1495B;
+    padding: 1% 10%;
+    margin: 0%;
+  }
+
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    display: block;
+    padding: 0;
+    margin: 0%;
+  }
+
+  a {
+    color: inherit;
+  }
+</style>
