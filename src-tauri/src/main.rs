a305cdcce865f11f59495e239502af85161c66fd
Lorenzo Ravaglia <lorenzo.ravaglia@hope.edu>
2023-03-29 09:13:02 -0400

    chore(backend): remove dead code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -17,19 +17,13 @@
     reason = "Make sure that the code adheres to best practices."
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {name}! You've been greeted from Rust!")
-}
-
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     pretty_env_logger::try_init()?;
     color_eyre::install()?;
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())?;
 
     Ok(())
