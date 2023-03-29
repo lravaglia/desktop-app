5c49f250260cd9afd1c480f25e401a6c7aae84ee
Lorenzo Ravaglia <lorenzo.ravaglia@hope.edu>
2023-03-29 08:23:09 -0400

    chore: setup basic testing

---
+++ tests/pass.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "vitest"
+
+
+describe("pass", () => {
+  test("one", () => {
+    expect(1).toBe(1)
+  })
+
+  test("true", () => {
+    expect(true).toBe(true)
+  })
+})
